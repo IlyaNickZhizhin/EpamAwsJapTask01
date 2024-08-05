d11ded5b2a048bec58ac9c783a92eaaa1422c3e8
ilya_zhizhin <ilya_zhizhin@epam.com>
2024-08-05 13:24:46 +0500

    task08-third try

--- task08/jsrc/main/java/com/task08/WeatherClient.java
+++ task08/jsrc/main/java/com/task08/WeatherClient.java
@@ -5,21 +5,16 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.RetentionSetting;
 import in.zhizh.WeatherRequest;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "weather_client",
 	roleName = "weather_client-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+	layers = {"weather_api"}
 )
-@LambdaLayer(layerName="weather_api",
-		libraries="../task08Layer/target/task08Layer-1.0-SNAPSHOT.jar")
+@LambdaLayer(
+		layerName = "weather_api",
+		libraries = {"lib/task08Layer-1.0-SNAPSHOT.jar"})
 @LambdaUrlConfig
 public class WeatherClient implements RequestHandler<Object, String> {
 
--- task08/resources.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-syndicate generate lambda --name "weather_client" --runtime "java"
-
-syndicate generate lambda_layer --name "weather_sdk" --runtime "java" --link_with_lambda "weather_client" --project_path "./"
