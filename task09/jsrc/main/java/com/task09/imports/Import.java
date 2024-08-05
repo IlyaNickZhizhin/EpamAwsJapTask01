27904e02f122a3e40fb69e534f285c46fa5d74a6
ilya_zhizhin <ilya_zhizhin@epam.com>
2024-08-05 15:42:03 +0500

    task09 - first try

--- task09/jsrc/main/java/com/task09/imports/Import.java
+++ task09/jsrc/main/java/com/task09/imports/Import.java
@@ -3,7 +3,7 @@ package com.task09.imports;
 
 public interface Import {
     String PREFIX = "cmtr-e28aa15d-";
-    String POSTFIX = "";
+    String POSTFIX = "-test";
     String TABLE_NAME = "Weather";
     String TABLE_FULL_NAME = PREFIX+TABLE_NAME+POSTFIX;
 }
