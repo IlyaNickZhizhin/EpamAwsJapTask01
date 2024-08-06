8af401ef056cbd1c98193693e5199b6c7e14f257
ilya_zhizhin <ilya_zhizhin@epam.com>
2024-08-06 19:56:42 +0500

    task10 - zero try

--- task09/jsrc/main/java/com/task09/imports/Import.java
+++ task09/jsrc/main/java/com/task09/imports/Import.java
@@ -3,7 +3,7 @@ package com.task09.imports;
 
 public interface Import {
     String PREFIX = "cmtr-e28aa15d-";
-    String POSTFIX = "-test";
+    String POSTFIX = "";
     String TABLE_NAME = "Weather";
     String TABLE_FULL_NAME = PREFIX+TABLE_NAME+POSTFIX;
 
--- task10/jsrc/main/java/com/task10/model/Table.java
+++ task10/jsrc/main/java/com/task10/model/Table.java
@@ -1,5 +1,6 @@
 package com.task10.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.task10.imports.Import;
 import lombok.Data;
@@ -7,7 +8,7 @@ import lombok.Data;
 @DynamoDBTable(tableName = Import.Tables_TABLE_FULL_NAME)
 @Data
 public class Table {
-
+    @DynamoDBHashKey(attributeName = "id")
     private int id;
     private int number;
     private int places;
