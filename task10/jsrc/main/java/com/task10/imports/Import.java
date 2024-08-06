f0a00373ce05199d14910d207617ecca21623da5
ilya_zhizhin <ilya_zhizhin@epam.com>
2024-08-06 21:20:12 +0500

    task10 - second try

--- task10/jsrc/main/java/com/task10/imports/Import.java
+++ task10/jsrc/main/java/com/task10/imports/Import.java
@@ -3,7 +3,7 @@ package com.task10.imports;
 
 public interface Import {
     String PREFIX = "cmtr-e28aa15d-";
-    String POSTFIX = "";
+    String POSTFIX = "-test";
     String Reservations_TABLE_NAME = "Reservations";
     String Tables_TABLE_NAME = "Tables";
     String COGNITO_USER_POOL_NAME = "simple-booking-userpool";
