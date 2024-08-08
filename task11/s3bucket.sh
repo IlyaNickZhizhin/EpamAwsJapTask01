dc3a7ffefaf741d70ad3dc069495743ec6907588
ilya_zhizhin <ilya_zhizhin@epam.com>
2024-08-08 13:49:42 +0500

    task11 - first try

--- task11/s3bucket.sh
+++ task11/s3bucket.sh
@@ -1,14 +1,4 @@
-syndicate generate meta s3_bucket \
-    --resource_name api-ui-hoster \
-    --location eu-central-1 \
-    --acl private \
-    --block_public_acls false \
-    --ignore_public_acls false \
-    --block_public_policy false \
-    --restrict_public_buckets false \
-	  --static_website_hosting true
-
 syndicate generate swagger_ui \
-	--name api_docs \
+	--name task11_api_ui \
 	--path_to_spec export/ltdgh1xsne_oas_v3.json \
 	--target_bucket api-ui-hoster
